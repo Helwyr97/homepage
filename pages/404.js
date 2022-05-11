68bd8c4989a4360293af349e98db0e770ab75c72
Helwyr97 <xoeloterotelmo@gmail.com>
2022-05-11 14:52:26 +0200

    change 404

--- pages/404.js
+++ pages/404.js
@@ -5,14 +5,31 @@ import {
     Heading,
     Container,
     Divider,
-    Button
+    Button,
+    Image,
+    SimpleGrid 
 } from '@chakra-ui/react'
 
 const NotFound = () => {
+    const img = `/images/robot.png`
     return (
-        <Container>
-            <Heading as="h1">404 - Not Found</Heading>
-            <Text>The page you&apos;re looking foor was not found</Text>
+        <Container maxW="container.md">
+            <SimpleGrid columns={2} spacing={0}>
+                <Box>
+                    <Image 
+                        src={img}
+                        width={60}
+                        height={60}
+                        alt="404"
+                        display="inline"
+                    />
+                </Box>
+                <Box mt={20}>
+                    <Heading as="h1">404 - Not Found</Heading>
+                    <Text>The page you&apos;re looking foor was not found</Text>
+                </Box>
+            </SimpleGrid>
+            
             <Divider my={6} />
             
             <Box my={6} align="center">
