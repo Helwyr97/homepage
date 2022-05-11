da463ba014f5c5344a5a37dc462d86ce335f1a64
Helwyr97 <xoeloterotelmo@gmail.com>
2022-05-11 04:03:54 +0200

    Navbar menu

---
+++ components/logo.js
@@ -0,0 +1,43 @@
+import Lin from "next/link"
+import Image from "next/image"
+import { Link, Text, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+const LogoBox = styled.span`
+    font-weight: bold;
+    font-size: 18px;
+    display: inline-flex;
+    align-items: center;
+    height: 30px;
+    line-height: 20px;
+    padding: 10px;
+    img {
+    transition: 200ms ease;
+    }
+    &:hover img {
+    transform: rotate(-20deg);
+    }
+`
+const Logo = () => {
+    const logoImg = `/images/logo${useColorModeValue('','-dark')}.png`
+    
+    return (
+        <Link href="/">
+            <a>
+                <LogoBox>
+                    <Image src={logoImg} width={30} height={30} alt="logo" />
+                    <Text
+                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                        fontFamily='Space Mono'
+                        fontWeight='bold'
+                        ml={3}
+                    >
+                        Xoel Otero
+                    </Text>
+                </LogoBox>
+            </a>
+        </Link>
+    )
+}
+
+export default Logo
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I'm a full-stack developer based in Coruña!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Xoel Otero
+                    </Heading>
+                </Box>
+            </Box>
+        </Container>
     )
 }
-
+ 
 export default Page
