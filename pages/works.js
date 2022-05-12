58d6c70473c62eb70144fa13b8fc55d9927cf83b
Helwyr97 <xoeloterotelmo@gmail.com>
2022-05-12 05:09:58 +0200

    start to add works

--- pages/works.js
+++ pages/works.js
@@ -2,8 +2,7 @@ import { Container, Heading, SimpleGrid, } from "@chakra-ui/react"
 import Section from '../components/section'
 import { WorkGridItem } from "../components/grid-item"
 import Layout from "../components/layouts/article"
-import thumbmusicdiscovererWeb from '../public/images/works/prueba.png'
-import thumbmusicdiscovererAndroid from '../public/images/works/prueba.png'
+import thumbmusicdiscovererWeb from '../public/images/works/musicdiscovererweb.png'
 
 const Works = () => {
     return (
@@ -15,15 +14,15 @@ const Works = () => {
 
                 <SimpleGrid columns={[1,1,2]} gap={6}>
                     <Section>
-                        <WorkGridItem id="musicdiscovererWeb" title="Music Discoverer Web" thumbnail={thumbmusicdiscovererWeb}>
+                        <WorkGridItem id="musicdiscovererweb" title="Music Discoverer Web" thumbnail={thumbmusicdiscovererWeb}>
                             Web app to discover music of all genres randomly, using the spotify api
                         </WorkGridItem>
                     </Section>
-                    <Section>
+                    {/* <Section>
                         <WorkGridItem id="musicdiscovererMobile" title="Music Discoverer Android" thumbnail={thumbmusicdiscovererAndroid}>
                             App to discover music of all genres randomly, using the spotify api
                         </WorkGridItem>
-                    </Section>
+                    </Section> */}
                 </SimpleGrid>
             </Container>
         </Layout>
---
+++ pages/works/musicdiscovererweb.js
@@ -0,0 +1,9 @@
+import { Container } from "@chakra-ui/react";
+
+const Work = () => (
+    <Container>
+        Under construction...
+    </Container>
+)
+
+export default Work
